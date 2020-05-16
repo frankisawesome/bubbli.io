8927e71ab08c23d5dc11a8e87ae57e149b25a788
Frank <b439988l@gmail.com>
2020-05-16 12:15:34 +1000

    firebase

--- src/app.tsx
+++ src/app.tsx
@@ -5,20 +5,23 @@ import { Login } from './pages/login';
 import { Home } from './pages/home';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import { FirebaseContextProvider } from './firebase/context';
 
 export const App: FC = () => {
   const [user, status] = useUser();
   return (
-    <Router history={createBrowserHistory()}>
-      <Nav {...status}></Nav>
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/login'>
-          <Login />
-        </Route>
-      </Switch>
-    </Router>
+    <FirebaseContextProvider>
+      <Router history={createBrowserHistory()}>
+        <Nav {...status}></Nav>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </FirebaseContextProvider>
   );
 };
